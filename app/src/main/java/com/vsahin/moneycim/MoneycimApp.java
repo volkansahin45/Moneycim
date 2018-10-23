9f028a841e92a4cdde4548318aa426b7eb987c96
Volkan Şahin <nihas.naklov@gmail.com>
2018-10-23 19:57:48 +0300

    Extend Application from DaggerApplication class

--- app/src/main/java/com/vsahin/moneycim/MoneycimApp.java
+++ app/src/main/java/com/vsahin/moneycim/MoneycimApp.java
@@ -10,28 +10,15 @@ import javax.inject.Inject;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
+import dagger.android.support.DaggerApplication;
 
 /**
  * Created by Volkan Şahin on 17.08.2017.
  */
 
-public class MoneycimApp extends Application implements HasActivityInjector {
-
-    @Inject
-    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
-
+public class MoneycimApp extends DaggerApplication {
     @Override
-    public void onCreate() {
-        super.onCreate();
-
-        DaggerAppComponent
-                .builder()
-                .create(this)
-                .inject(this);
-    }
-
-    @Override
-    public AndroidInjector<Activity> activityInjector() {
-        return dispatchingAndroidInjector;
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().create(this);
     }
 }
