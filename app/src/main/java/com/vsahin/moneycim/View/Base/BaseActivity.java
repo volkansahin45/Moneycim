fd909512e64c4a9065261804dd18018dcfbcee50
Volkan Şahin <nihas.naklov@gmail.com>
2019-01-11 09:53:49 +0300

    Use DaggerACtivity and DaggerFragment

--- app/src/main/java/com/vsahin/moneycim/View/Base/BaseActivity.java
+++ app/src/main/java/com/vsahin/moneycim/View/Base/BaseActivity.java
@@ -1,25 +1,16 @@
 package com.vsahin.moneycim.View.Base;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.appcompat.app.AppCompatActivity;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-import dagger.android.AndroidInjection;
-import dagger.android.AndroidInjector;
-import dagger.android.DispatchingAndroidInjector;
-import dagger.android.support.HasSupportFragmentInjector;
 
 import com.vsahin.moneycim.R;
 
-import javax.inject.Inject;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import butterknife.Unbinder;
+import dagger.android.support.DaggerAppCompatActivity;
 
-public abstract class BaseActivity  extends AppCompatActivity implements HasSupportFragmentInjector {
-
-    @Inject
-    DispatchingAndroidInjector<Fragment> fragmentInjector;
+public abstract class BaseActivity extends DaggerAppCompatActivity {
 
     protected Unbinder unbinder;
 
@@ -27,7 +18,6 @@ public abstract class BaseActivity  extends AppCompatActivity implements HasSupp
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
 
         fragmentManager = getSupportFragmentManager();
@@ -39,7 +29,7 @@ public abstract class BaseActivity  extends AppCompatActivity implements HasSupp
         unbinder.unbind();
     }
 
-    protected void showRootFragment(Fragment fragment){
+    protected void showRootFragment(Fragment fragment) {
         if(getFragmentBackStackCount() == 0) {
             fragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, fragment)
@@ -47,15 +37,15 @@ public abstract class BaseActivity  extends AppCompatActivity implements HasSupp
         }
     }
 
-    public void showFragment(Fragment nextFragment){
+    public void showFragment(Fragment nextFragment) {
         //be sure to not load same fragment
-        if(isLastFragmentInBackstack(nextFragment)){
+        if(isLastFragmentInBackstack(nextFragment)) {
             return;
         }
 
         fragmentManager.beginTransaction()
                 .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out, android.R.anim.fade_in, android.R.anim.fade_out)
-                .replace(R.id.fragment_container , nextFragment)
+                .replace(R.id.fragment_container, nextFragment)
                 .addToBackStack(nextFragment.getClass().getName())
                 .commit();
     }
@@ -66,16 +56,11 @@ public abstract class BaseActivity  extends AppCompatActivity implements HasSupp
         return getFragmentBackStackCount() != 0 && getLastFragmentNameInBackStack().equals(nextFragmentName);
     }
 
-    private String getLastFragmentNameInBackStack(){
+    private String getLastFragmentNameInBackStack() {
         return fragmentManager.getBackStackEntryAt(getFragmentBackStackCount() - 1).getName();
     }
 
-    protected int getFragmentBackStackCount(){
+    protected int getFragmentBackStackCount() {
         return fragmentManager.getBackStackEntryCount();
-    }
-
-    @Override
-    public AndroidInjector<Fragment> supportFragmentInjector() {
-        return fragmentInjector;
     }
 }
--- app/src/main/java/com/vsahin/moneycim/View/Base/BaseFragment.java
+++ app/src/main/java/com/vsahin/moneycim/View/Base/BaseFragment.java
@@ -1,17 +1,19 @@
 package com.vsahin.moneycim.View.Base;
 
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import dagger.android.support.DaggerFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public abstract class BaseFragment extends Fragment{
+public abstract class BaseFragment extends DaggerFragment {
     protected Unbinder unbinder;
 
     @Nullable
